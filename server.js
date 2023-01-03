856c349a68804764858eded8a78178b77047c67c
Nick Klassen <nick.klassen@smail.th-koeln.de>
2023-01-03 13:33:15 +0100

    create route for cars

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const fs = require("fs");
+const fetch = require("node-fetch");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", function (req, res) {
   res.render("index");
+});
+
+app.get("/car", function (req, res) {
+  fs.readFile("car/car.json", "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    var obj = JSON.parse(data);
+    res.json(obj);
+  });
 });
 
 app.listen(port);
