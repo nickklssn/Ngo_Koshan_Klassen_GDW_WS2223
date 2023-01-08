81b859807cbe3f4ebe9bf63dfb7fa7c1653cea42
Nick Klassen <nick.klassen@smail.th-koeln.de>
2023-01-08 15:56:58 +0100

    fix brace error

--- index.js
+++ index.js
@@ -12,66 +12,62 @@ function initMap() {
   roadDirection();
   calculateTime();
 
-  
   var autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('origin'),
-    { types: ['geocode'] }
-
-    
+    document.getElementById("origin"),
+    { types: ["geocode"] }
   );
   var autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('dest'),
-    { types: ['geocode'] }
+    document.getElementById("dest"),
+    { types: ["geocode"] }
   );
   function roadDirection() {
-  // Create a DirectionsService object to request directions
-  var directionsService = new google.maps.DirectionsService();
+    // Create a DirectionsService object to request directions
+    var directionsService = new google.maps.DirectionsService();
 
-  // Create a DirectionsRenderer object to display the directions
-  var directionsRenderer = new google.maps.DirectionsRenderer();
-  directionsRenderer.setMap(map);  // The map to display the directions on
+    // Create a DirectionsRenderer object to display the directions
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map); // The map to display the directions on
 
-  // Set the origin and destination for the directions
-  var request = {
-    origin: 'Köln, Germany',
-    destination: 'Gummersbach, Germany',
-    travelMode: 'DRIVING'  // Mode of transport
-  };
+    // Set the origin and destination for the directions
+    var request = {
+      origin: "Köln, Germany",
+      destination: "Gummersbach, Germany",
+      travelMode: "DRIVING", // Mode of transport
+    };
 
-  // Request the directions and render them on the map
-  directionsService.route(request, function(response, status) {
-    if (status == 'OK') {
-      directionsRenderer.setDirections(response);
-    }
-  });
-}
+    // Request the directions and render them on the map
+    directionsService.route(request, function (response, status) {
+      if (status == "OK") {
+        directionsRenderer.setDirections(response);
+      }
+    });
+  }
 
+  function calculateTime() {
+    // Create a DirectionsService object to request directions
+    var directionsService = new google.maps.DirectionsService();
 
-function calculateTime() {
-  // Create a DirectionsService object to request directions
-  var directionsService = new google.maps.DirectionsService();
+    // Set the origin and destination for the directions
+    var request = {
+      origin: "Köln, DE",
+      destination: "Gummersbach, DE",
+      travelMode: "DRIVING", // Mode of transport
+    };
 
-  // Set the origin and destination for the directions
-  var request = {
-    origin: 'Köln, DE',
-    destination: 'Gummersbach, DE',
-    travelMode: 'DRIVING'  // Mode of transport
-  };
+    // Request the directions and get the duration
+    directionsService.route(request, function (response, status) {
+      if (status == "OK") {
+        // Get the duration in seconds
+        var duration = response.routes[0].legs[0].duration.value;
 
-  // Request the directions and get the duration
-  directionsService.route(request, function(response, status) {
-    if (status == 'OK') {
-      // Get the duration in seconds
-      var duration = response.routes[0].legs[0].duration.value;
+        // Convert the duration to hours and minutes
+        var hours = Math.floor(duration / 3600);
+        var minutes = Math.floor((duration % 3600) / 60);
 
-      // Convert the duration to hours and minutes
-      var hours = Math.floor(duration / 3600);
-      var minutes = Math.floor((duration % 3600) / 60);
-
-      console.log('Duration: ' + hours + 'h ' + minutes + 'm');
-    }
-  });
-}
+        console.log("Duration: " + hours + "h " + minutes + "m");
+      }
+    });
+  }
 
   infoWindow = new google.maps.InfoWindow();
 
@@ -79,7 +75,6 @@ function calculateTime() {
 
   // This function sets a custom marker depending on user input
   function setCustomMarker() {
-
     let marker = new google.maps.Marker({
       position: null,
       map: map,
@@ -94,7 +89,7 @@ function calculateTime() {
 
     map.setCenter(pos);
   }
-// find the current Position automaticly
+  // find the current Position automaticly
   subButton.addEventListener("click", setCustomMarker);
 
   const locationButton = document.createElement("button");
@@ -130,72 +125,80 @@ function calculateTime() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
-  
+
   //This function handles error if geolocation does not work
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
 
+    // Elevations API //
 
+    // Function to calculate average gain of a route
+    function getAverageElevationGain(startLat, startLng, endLat, endLng) {
+      // Create elevation object
+      var elevator = new google.maps.ElevationService({
+        apiKey: "AIzaSyA6PpTUvfOJ0l3P4ZtlOpSs2zMKMtZ57I0",
+      });
+      // Set start and end point
+      var routeCoordinates = [
+        { lat: startLat, lng: startLng }, // Start
+        { lat: endLat, lng: endLng }, // End
+      ];
 
-// Elevations API //
+      // Define the path by assign latitude and longitude
+      var path = routeCoordinates.map(
+        (coordinate) => new google.maps.LatLng(coordinate.lat, coordinate.lng)
+      );
 
+      // Create a Request
+      var request = {
+        path: path,
+        samples: 256, // Amount of samples
+      };
 
-  // Function to calculate average gain of a route
-  function getAverageElevationGain(startLat, startLng, endLat, endLng) {
-    // Create elevation object
-    var elevator = new google.maps.ElevationService({
-      apiKey: 'AIzaSyA6PpTUvfOJ0l3P4ZtlOpSs2zMKMtZ57I0'
-    });
-    // Set start and end point
-    var routeCoordinates = [
-      {lat: startLat, lng: startLng},  // Start
-      {lat: endLat, lng: endLng}       // End
-    ];
-
-    // Define the path by assign latitude and longitude
-    var path = routeCoordinates.map(coordinate => new google.maps.LatLng(coordinate.lat, coordinate.lng));
-
-    // Create a Request
-    var request = {
-      path: path,
-      samples: 256  // Amount of samples
-    };
-
-    // Do a request along the path to get heights
-    elevator.getElevationAlongPath(request, function(results, status) {
-      if (status === 'OK') {
-        // Calculate the average gain of the route
-        var totalElevationGain = 0;
-        var previousElevation = 0;
-        for (var i = 0; i < results.length; i++) {
-          var elevation = results[i].elevation;
-          if (i > 0) {
-            totalElevationGain += Math.max(0, elevation - previousElevation);
+      // Do a request along the path to get heights
+      elevator.getElevationAlongPath(request, function (results, status) {
+        if (status === "OK") {
+          // Calculate the average gain of the route
+          var totalElevationGain = 0;
+          var previousElevation = 0;
+          for (var i = 0; i < results.length; i++) {
+            var elevation = results[i].elevation;
+            if (i > 0) {
+              totalElevationGain += Math.max(0, elevation - previousElevation);
+            }
+            previousElevation = elevation;
           }
-          previousElevation = elevation;
+          var averageElevationGain = totalElevationGain / (results.length - 1);
+          var roundAverageElevationGain = averageElevationGain.toFixed(2);
+          console.log(
+            "Die durchschnittliche Steigung der Route beträgt: " +
+              roundAverageElevationGain +
+              " Meter pro Kilometer."
+          );
+        } else {
+          console.error(
+            "Es ist ein Fehler beim Abrufen der Höhenpunkte aufgetreten: " +
+              status
+          );
         }
-        var averageElevationGain = totalElevationGain / (results.length - 1);
-        var roundAverageElevationGain = averageElevationGain.toFixed(2);
-        console.log("Die durchschnittliche Steigung der Route beträgt: " + roundAverageElevationGain + " Meter pro Kilometer.");
-      } else {
-        console.error("Es ist ein Fehler beim Abrufen der Höhenpunkte aufgetreten: " + status);
-      }
-    });
-  }
+      });
+    }
 
-  // Test from Cologne -> Gummersbach
-  getAverageElevationGain(50.94148075038749, 6.958224297010802, 51.02304632512543, 7.561820898187938);
+    // Test from Cologne -> Gummersbach
+    getAverageElevationGain(
+      50.94148075038749,
+      6.958224297010802,
+      51.02304632512543,
+      7.561820898187938
+    );
+  }
 }
 
 window.initMap = initMap;
 // Rufe die Funktion auf und übergebe Start- und Endpunkt-Koordinaten
-
-
-
-
