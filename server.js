9e1e8094d24e096cc2428d3912d85c70d942c6fc
Nick Klassen <nick.klassen@smail.th-koeln.de>
2023-01-15 20:02:57 +0100

    add comments

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ const Ajv = require("ajv");
 const ajv = new Ajv();
 const carSchema = require("./schema/carSchema.js");
 const driverSchema = require("./schema/driverSchema.js");
-const { raw } = require("express");
 const { exec } = require("child_process");
 
+//Middleware for express
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
+//GET google map
 app.get("/", function (req, res) {
   res.render("index");
 });
 
+//GET sight nearby depending on current user
 app.get("/sight", function (req, res) {
+  //read json file and parse data
   fs.readFile("data/currSightsReq.json", "utf-8", (err, data) => {
     if (err) throw err;
 
@@ -27,7 +30,9 @@ app.get("/sight", function (req, res) {
   });
 });
 
+//GET all cars
 app.get("/car", function (req, res) {
+  //read json file if exists and parse data
   if (fs.existsSync("data/car.json")) {
     fs.readFile("data/car.json", "utf-8", (err, data) => {
       if (err) {
@@ -42,74 +47,32 @@ app.get("/car", function (req, res) {
   }
 });
 
+//GET one specific car
 app.get("/car/:id", async function (req, res) {
   try {
-    // Use fs.promises.readFile() to read the contents of the 'cars.json' file
+    //read json file async and parse data
     const data = await fs.promises.readFile("data/car.json", "utf8");
-    // Parse the contents of the file as JSON
     const cars = JSON.parse(data);
 
-    // Use Object.entries() to extract the key-value pairs of the object
-    // Use Array.prototype.find() method to find the key-value pair with a matching key
+    //search for matched car
     const car = Object.entries(cars).find(
       ([key, value]) => key == req.params.id
     );
 
-    // If a matching car is not found
     if (!car) {
-      // Send a 404 status code and message to the client
       res.status(404).send("Car not found.");
     } else {
-      // If a matching car is found, Send the car object to the client as a JSON response
+      //return matched car
       res.json(car[1]);
     }
   } catch (err) {
-    // If an error occurs while reading the file, send a 500 status code and error message to the client
     res.status(500).send(`Error reading cars.json file: ${err}`);
   }
 });
-//Does not work yet
-app.delete("/car/:id", (req, res) => {
-  const id = req.params.id;
-  // read the json file
-  fs.readFile("data/car.json", "utf-8", (err, data) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        res.status(404).send({ error: "car.json not found" });
-      } else {
-        res.status(500).send({ error: err });
-      }
-      return;
-    }
-    try {
-      const cars = JSON.parse(data);
-      // find the index of the car object that matches the id
-      const index = cars.findIndex((car) => car.id == id);
-      if (index === -1) {
-        res.status(404).send({ error: "Car not found" });
-        return;
-      }
-      // remove the car object from the array
-      cars.splice(index, 1);
-      // write the new cars array to the json file
-      fs.writeFile("data/car.json", JSON.stringify(cars), (err) => {
-        if (err) {
-          res.status(500).send({ error: err });
-          return;
-        }
-        res.send({ message: `Car with id ${id} is deleted` });
-      });
-    } catch (err) {
-      if (err instanceof SyntaxError) {
-        res.status(500).send({ error: "car.json is not a valid JSON file" });
-      } else {
-        res.status(500).send({ error: err });
-      }
-    }
-  });
-});
 
+//DELETE all cars
 app.delete("/car", function (req, res) {
+  //delete json file
   fs.unlink("data/car.json", (err) => {
     if (err) {
       throw err;
@@ -118,20 +81,20 @@ app.delete("/car", function (req, res) {
   });
 });
 
+//POST new car
 app.post("/car", function (req, res) {
-  // Validate the request body against the carSchema
+  //validate with carschema
   const validate = ajv.compile(carSchema);
   const valid = validate(req.body);
 
-  // If the request body is not valid, send a 400 status with the validation errors
   if (!valid) {
     res.status(400).send(validate.errors);
   } else {
-    // Check if the data.json file exists
+    // Check if the json file exists
     fs.access("data/car.json", fs.constants.F_OK, (err) => {
       let jsonData = {};
       if (!err) {
-        // Read the data.json file
+        // Read the json file
         fs.readFile("data/car.json", "utf8", (err, data) => {
           if (err) {
             console.log(err);
@@ -141,7 +104,7 @@ app.post("/car", function (req, res) {
           }
           // Append the new car object to the existing data object
           jsonData = { ...jsonData, [req.body.carId]: req.body };
-          // Write the updated data object to the data.json file
+          // Write the updated data object to the json file
           fs.writeFile("data/car.json", JSON.stringify(jsonData), (err) => {
             if (err) {
               console.log(err);
@@ -151,7 +114,7 @@ app.post("/car", function (req, res) {
         });
       } else {
         jsonData = { [req.body.carId]: req.body };
-        // Write the new data object to the data.json file
+        // Write the new data object to the json file
         fs.writeFile("data/car.json", JSON.stringify(jsonData), (err) => {
           if (err) {
             console.log(err);
@@ -163,20 +126,20 @@ app.post("/car", function (req, res) {
   }
 });
 
+//POST new driver
 app.post("/driver", function (req, res) {
-  // Validate the request body against the driverSchema
+  //validate with driverSchema
   const validate = ajv.compile(driverSchema);
   const valid = validate(req.body);
 
-  // If the request body is not valid, send a 400 status with the validation errors
   if (!valid) {
     res.status(400).send(validate.errors);
   } else {
-    // Check if the data.json file exists
+    // Check if the json file exists
     fs.access("data/driver.json", fs.constants.F_OK, (err) => {
       let jsonData = {};
       if (!err) {
-        // Read the data.json file
+        // Read the json file
         fs.readFile("data/driver.json", "utf8", (err, data) => {
           if (err) {
             console.log(err);
@@ -186,7 +149,7 @@ app.post("/driver", function (req, res) {
           }
           // Append the new driver object to the existing data object
           jsonData = { ...jsonData, [req.body.driverId]: req.body };
-          // Write the updated data object to the data.json file
+          // Write the updated data object to the json file
           fs.writeFile("data/driver.json", JSON.stringify(jsonData), (err) => {
             if (err) {
               console.log(err);
@@ -196,7 +159,7 @@ app.post("/driver", function (req, res) {
         });
       } else {
         jsonData = { [req.body.driverId]: req.body };
-        // Write the new data object to the data.json file
+        // Write the new data object to the json file
         fs.writeFile("data/driver.json", JSON.stringify(jsonData), (err) => {
           if (err) {
             console.log(err);
@@ -208,7 +171,9 @@ app.post("/driver", function (req, res) {
   }
 });
 
+//GET all driver
 app.get("/driver", function (req, res) {
+  //read json file if exists and parse data
   if (fs.existsSync("data/driver.json")) {
     fs.readFile("data/driver.json", "utf-8", (err, data) => {
       if (err) {
@@ -222,16 +187,21 @@ app.get("/driver", function (req, res) {
   }
 });
 
+//GET one specific driver
 app.get("/driver/:id", async function (req, res) {
   try {
+    //read json file async and parse data
     const data = await fs.promises.readFile("data/driver.json", "utf8");
     const drivers = JSON.parse(data);
+
+    //search for matched driver
     const driver = Object.entries(drivers).find(
       ([key, value]) => key == req.params.id
     );
     if (!driver) {
       res.status(404).send("Driver not found.");
     } else {
+      //return driver
       res.json(driver[1]);
     }
   } catch (err) {
@@ -239,28 +209,9 @@ app.get("/driver/:id", async function (req, res) {
   }
 });
 
-app.delete("/driver/:id", async function (req, res) {
-  try {
-    const data = await fs.promises.readFile("data/driver.json", "utf-8");
-    const drivers = JSON.parse(data);
-    const notMatchedDriver = Object.fromEntries(drivers).find(
-      ([key, value]) => key != req.params.id
-    );
-    console.log(notMatchedDriver);
-    if (!notMatchedDriver) {
-      res.status(404).send("No driver available");
-    } else {
-      fs.writeFile("data/car.json", JSON.stringify(notMatchedDriver), (err) => {
-        if (err) {
-          console.log(err);
-        }
-        res.send("Object deleted successfully");
-      });
-    }
-  } catch {}
-});
-
+//DELETE all driver
 app.delete("/driver", function (req, res) {
+  //delete json file
   fs.unlink("data/driver.json", (err) => {
     if (err) {
       throw err;
@@ -269,20 +220,22 @@ app.delete("/driver", function (req, res) {
   });
 });
 
+//EXECUTE script for calculation
 app.get("/insertData/:id", (req, res) => {
   let id = req.params.id;
   exec("npm run start " + id, (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return res.status(500).send(error.message);
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return res.status(500).send(stderr);
-      }
-      res.status(200).send("Script executed successfully");
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return res.status(500).send(error.message);
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return res.status(500).send(stderr);
+    }
+    res.status(200).send("Script executed successfully");
   });
 });
 
+//Server listen on port 3000
 app.listen(port);
 console.log(`App listen on port ${port}`);
